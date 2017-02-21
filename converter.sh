75b92a7db463882a62568f3eab0a406e65136853
OstKh <harysh@ucu.edu.ua>
2017-02-21 21:30:00 +0200

    Update converter.sh

--- converter.sh
+++ converter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#default path is one where the file is run.
+#the default path is place where the file placed
 declare count=0
 declare indices=8
 
-while getopts 'p:i:c:' opt; do #gives an ability to specify the path and the unique number starting point
+while getopts 'p:i:c:' opt; do #gives an ability to specify the path, the unique number and the starting point
   case $opt in
     p)
-      echo "PATH: " $OPTARG
+      echo "PATH: " $OPTARG #
       cd
       cd $OPTARG
       echo " -p: Path chosen!" >&2
@@ -29,8 +29,8 @@ done
 for image in *;
 do
   today=`date '+%S_%M_%H-%d-%m-%Y'` #takes data of current time
-  if [ ${image: -4} == ".jpg" ] || [ ${image: -5} == ".jpeg" ] || 
-     [ ${image: -4} == ".bmp" ] || [ ${image: -4} == ".png" ] || [ ${image: -4} == ".gif" ];
+  if [ "${image: -4}" == ".jpg" ] || [ "${image: -5}" == ".jpeg" ] || 
+     [ "${image: -4}" == ".bmp" ] || [ "${image: -4}" == ".png" ] || [ "${image: -4}" == ".gif" ];
      then
          j=`printf "%0*d" $indices $count` # creates starting uniqe number
          let count+=1 
@@ -39,7 +39,6 @@ do
                  convert -strip $image[0] "${today%}-$j.jpg" #converts one picture from .gif
          else
              convert $image "${today%}-$j.jpg"
-     #rm $image  # if removing image that was converted
          fi
+     rm $image  # removing image that was under converting
   fi		
-done
